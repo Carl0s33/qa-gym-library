b0146b4c6539dc6e5d5a50006f2cbe872a879587
Luana Gomes <luanagomes.contato1@gmail.com>
2026-06-18 15:34:45 -0300

    Refactor BookValidatorTest for improved readability

--- src/test/java/br/edu/ifrn/qagym/util/BookValidatorTest.java
+++ src/test/java/br/edu/ifrn/qagym/util/BookValidatorTest.java
@@ -1,10 +1,12 @@
 package br.edu.ifrn.qagym.util;
+
 import br.edu.ifrn.qagym.model.Book;
 import org.junit.jupiter.api.Test;
 
 import java.time.Year;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BookValidatorTest {
 
@@ -12,24 +14,14 @@ class BookValidatorTest {
 
     @Test
     void deveRetornarFalseQuandoAnoForNegativo() {
-        Book book = new Book(
-                "123",
-                "Código Limpo",
-                "Robert Martin",
-                -1
-        );
+        Book book = new Book("123", "Código Limpo", "Robert Martin", -1);
 
         assertFalse(validator.isValid(book));
     }
 
     @Test
     void deveRetornarFalseQuandoAnoForZero() {
-        Book book = new Book(
-                "123",
-                "Código Limpo",
-                "Robert Martin",
-                0
-        );
+        Book book = new Book("123", "Código Limpo", "Robert Martin", 0);
 
         assertFalse(validator.isValid(book));
     }
@@ -56,51 +48,17 @@ class BookValidatorTest {
         );
 
         assertTrue(validator.isValid(book));
-
-import br.edu.ifrn.qagym.model.Book;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-
-public class BookValidatorTest {
-    private BookValidator validator;
-
-    @BeforeEach
-    void setUp() {
-
-        validator = new BookValidator();
     }
 
     @Test
     void deveRetornarFalsoQuandoAutorForNulo() {
+        Book livroAutorNulo = new Book(
+                "12345",
+                "O hobbit",
+                null,
+                1902
+        );
 
-        Book livroAutorNulo = new Book("12345", "O hobbit", null, 1902);
-
-        boolean resultado = validator.isValid(livroAutorNulo);
-
-        assertFalse(resultado, "O validador deveria rejeitar um autor nulo.");
-    }
-
-    @Test
-    void deveRetornarFalsoQuandoAutorForVazio() {
-
-        Book livroAutorVazio = new Book("12345", "O hobbit", "", 1902);
-
-        boolean resultado = validator.isValid(livroAutorVazio);
-
-        assertFalse(resultado, "O validador deveria rejeitar um autor em branco.");
-    }
-
-    @Test
-    void deveRetornarVerdadeiroQuandoAutorValido() {
-
-        Book livroComAutor = new Book("12345", "O hobbit", "João Pedro", 1902);
-
-
-        boolean resultado = validator.isValid(livroComAutor);
-        assertTrue(resultado, "O validador deveria aceitar um autor válido.");
+        assertFalse(validator.isValid(livroAutorNulo));
     }
 }
